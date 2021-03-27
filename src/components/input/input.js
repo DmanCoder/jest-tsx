681f496b03925215bd24441cce9338df142b39bf
DmanCoder <ktdenis.1@live.com>
2021-03-27 11:49:21 +1000

    base styles

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -2,7 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secreteWord }) => {
-  return <div data-test="input-component" className=""></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test="input-component" className="">
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+
+        <button data-test="submit-bottom" className="btn btn-primary mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
